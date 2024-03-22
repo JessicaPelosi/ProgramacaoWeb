9f85e0d4ac0c270af50100c8df4c5bf1b4bb24cb
JessicaPelosi <je.simada@hotmail.com>
2024-03-22 20:01:12 -0300

    Include exer1

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,8 @@ require __DIR__."/vendor/autoload.php";
 $metodo = $_SERVER['REQUEST_METHOD'];
 $caminho = $_SERVER['PATH_INFO']  ?? '/';
 
+#use Php\Primeiroprojeto\Router
+
 $r = new Php\Primeiroprojeto\Router($metodo, $caminho);
 
 #ROTAS
@@ -17,6 +19,16 @@ $r->get('/olapessoa/{nome}', function($params){
     return 'Olá ' . $params[1];
 });
 
+$r->get('/exer1/formulario', function(){
+    include("exer1.html");
+});
+
+$r->post('/exer1/resposta', function(){
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $soma = $valor1 + $valor2;
+    return "A soma é: {$soma}";
+});
 #ROTAS
 
 $resultado = $r->handler();
